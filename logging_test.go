2ec1c29e877e3908cfdeee9eb0b1dcc497f5c206
Laurent Monin <github@norz.org>
2025-11-08 01:27:19 +0100

    Test LogOutput()

--- logging_test.go
+++ logging_test.go
@@ -51,3 +51,47 @@ func TestSetLogLevel(t *testing.T) {
 		})
 	}
 }
+
+func TestLogOutput(t *testing.T) {
+	// --- Setup ---
+	var logMutex sync.Mutex
+	var capturedLog string
+	originalLogFunc := LogOutput
+	LogOutput = func(level LogLevel, tag string, format string, v ...interface{}) {
+		// The mock must replicate the behavior of the real function.
+		if level <= CurrentLogLevel {
+			logMutex.Lock()
+			capturedLog = "logged"
+			logMutex.Unlock()
+		}
+	}
+	t.Cleanup(func() {
+		LogOutput = originalLogFunc
+		CurrentLogLevel = LevelWarning // Reset to default
+	})
+
+	tests := []struct {
+		name          string
+		messageLevel  LogLevel
+		currentLevel  LogLevel
+		expectLogging bool
+	}{
+		{"Should Log - Level Warning, Current Warning", LevelWarning, LevelWarning, true},
+		{"Should Log - Level Error, Current Info", LevelError, LevelInfo, true},
+		{"Should NOT Log - Level Debug, Current Info", LevelDebug, LevelInfo, false},
+		{"Should NOT Log - Level Info, Current Warning", LevelInfo, LevelWarning, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			capturedLog = "" // Reset for each run
+			CurrentLogLevel = tt.currentLevel
+			// Call the public LogOutput function, which has been mocked, not the internal one.
+			LogOutput(tt.messageLevel, "TEST", "test message")
+
+			if (capturedLog != "") != tt.expectLogging {
+				t.Errorf("Expected logging to be %v, but it was %v", tt.expectLogging, (capturedLog != ""))
+			}
+		})
+	}
+}
